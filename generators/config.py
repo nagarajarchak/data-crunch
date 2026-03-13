8240585922de8ea2c04e505a0d13a71bf02ca69a
nagarajarchak <archak.nagaraj@gmail.com>
2026-03-13 20:44:22 +0530

    Config segregationn

--- generators/config.py
+++ generators/config.py
@@ -1,7 +1,9 @@
 import logging
 from enum import Enum
 
-class Config(Enum):
+class ErrorMessages(Enum):
     SUCCESS = "success"
     FAILED = "failed"
+
+class Kafka(Enum):    
     LOCALHOST = "localhost:9092"
